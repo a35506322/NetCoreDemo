65828d9ecbee4c5bcce3996e3ff8e83401053d20
理竣 莊 <andy28572463@gmail.com>
2023-11-15 16:58:05 +0800

    feat: 新增throw error 情境

---
+++ ThrowError差異/Program.cs
@@ -0,0 +1,80 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+try
+{
+    ThrowNewError();
+
+}
+catch (Exception ex)
+{
+    Console.WriteLine("ThrowNewError"+ ex.ToString());
+}
+
+Console.WriteLine();
+
+try
+{
+    ThrowError();
+}
+catch (Exception ex)
+{
+    Console.WriteLine("ThrowError"+ ex.ToString());
+}
+
+
+Console.WriteLine();
+
+try
+{
+    Throw();
+}
+catch (Exception ex)
+{
+    Console.WriteLine("Throw"+ ex.ToString());
+}
+
+Console.ReadLine();
+
+
+static void Funcion ()
+{ 
+    throw new Exception ("Throw Funcion Error");
+}
+
+static void ThrowNewError()
+{
+    try
+    {
+        Funcion();
+    }
+    catch (Exception ex) 
+    {
+        throw new Exception(ex.ToString());
+    }
+}
+
+static void ThrowError()
+{
+    try
+    {
+        Funcion();
+    }
+    catch (Exception ex)
+    {
+        throw ex;
+    }
+}
+
+
+static void Throw()
+{
+    try
+    {
+        Funcion();
+    }
+    catch
+    {
+        throw;
+    }
+}
+
